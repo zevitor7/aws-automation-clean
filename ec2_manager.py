9617449c512ba8f5d976fd33092349dd12b986d5
Santos Jose <jose.xzx.santos@scania.com>
2026-04-06 17:00:40 -0300

    fix session boto3

--- ec2_manager.py
+++ ec2_manager.py
@@ -1,27 +1,15 @@
 import boto3
 import sys
 
-ec2 = session.client('ec2')
+ec2 = boto3.client('ec2')
 
 action = sys.argv[1]
-instance_id = sys.argv[2] if len(sys.argv) > 2 else None
+instance_id = sys.argv[2]
 
 if action == "start":
     ec2.start_instances(InstanceIds=[instance_id])
-    print(f"Instância {instance_id} iniciada!")
+    print("Instância iniciada!")
 
 elif action == "stop":
     ec2.stop_instances(InstanceIds=[instance_id])
-    print(f"Instância {instance_id} parada!")
-
-elif action == "create":
-    response = ec2.run_instances(
-        ImageId='ami-0c55b159cbfafe1f0',
-        InstanceType='t2.micro',
-        MinCount=1,
-        MaxCount=1
-    )
-    print("Instância criada!", response['Instances'][0]['InstanceId'])
-
-else:
-    print("Ação inválida!")
+    print("Instância parada!")
